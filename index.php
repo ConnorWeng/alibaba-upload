10978aac16875a2e44ba76f5394116b7e1335437
Connor Weng <foamtea30@gmail.com>
2013-08-31 22:19:44 +0800

    Make redirect url configurable.

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
+<?php
+require_once 'Config.class.php';
+?>
+
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <script>
 function s() {
-    var url_template = "http://gw.open.1688.com/auth/authorize.htm?client_id=1006478&site=china&redirect_uri=http%3a%2f%2flocalhost%2fphp%2fauth.php&state=YOUR_PARM&_aop_signature=DC667B0B8D0A5ADA0A43877803CCC3292CF1521A",
+    var url_template = "http://gw.open.1688.com/auth/authorize.htm?client_id=1006478&site=china&redirect_uri=<?php echo(Config::get('redirect_uri')); ?>&state=YOUR_PARM&_aop_signature=DC667B0B8D0A5ADA0A43877803CCC3292CF1521A",
     url = url_template.replace('YOUR_PARM', document.mainform.title.value);
     window.location = 'sign.php?url=' + url;
 }
